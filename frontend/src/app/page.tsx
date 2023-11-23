af2ef909a3a6a5da5b6657dba185cbd56dec8fe3
Eugene Chan <15060312+eugenechantk@users.noreply.github.com>
2023-11-23 17:51:46 -0500

    [wip] FE connecting to BE websocket server

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,16 +1,22 @@
-import Image from "next/image";
+const socket = new WebSocket('ws://localhost:8080');
+socket.onopen = () => {
+  console.log('connected to server');
+};
+
+socket.onmessage = (event) => {
+  console.log('message received: ' + event.data);
+};
 
 async function handleSubmit(formData: FormData) {
   'use server';
   console.log('submitting form...')
   const message = formData.get('message');
+  socket.send(message ? message : '');
   // Perform server-side operations with the message here
   console.log(message);
 }
 
-
 export default function Home() {
-  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <form action={handleSubmit}>
--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,14 @@
 import ws from 'ws';
 
-const server = new ws.Server({ port: 8080 });
+const WS_PORT = 8080;
+
+const server = new ws.Server({ port: WS_PORT });
+
+console.log(`Websocket server started on port ${WS_PORT}`);
 
 server.on('connection', (socket) => {
-  socket.on('message', (message) => {
+  console.log(`New connection from ${socket.url}`);
+  socket.on('message', (message: string) => {
     console.log(`Received: ${message}`);
     socket.send(`${message}`)
   });
