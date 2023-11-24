2ce8a51bab524a96505406c64d4d0fdad8d5cbd0
Eugene Chan <15060312+eugenechantk@users.noreply.github.com>
2023-11-24 02:25:19 -0500

    fix nextjs railway deploy issue

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
 // Connect to web socket
-const socket = io("ws://localhost:3001");
+const socket = io(process.env.NEXT_PUBLIC_WS_URL as string);
 
 export default socket;
