c2bd5de7e0a178c2ace801cc623b40af42960e10
Eugene Chan <15060312+eugenechantk@users.noreply.github.com>
2023-11-24 00:06:22 -0500

    changed to socket.io

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,19 +1,32 @@
-const socket = new WebSocket('ws://localhost:8080');
-socket.onopen = () => {
-  console.log('connected to server');
-};
+import { io } from "socket.io-client";
 
-socket.onmessage = (event) => {
-  console.log('message received: ' + event.data);
-};
+const socket = io("ws://localhost:3500");
+
+socket.on("connect", () => {
+  console.log("connected to server");
+  socket.send("hello from client");
+})
+
+socket.on("message", (message) => {
+  console.log("message received: " + message);
+})
+
+// socket.onopen = () => {
+//   console.log("connected to server");
+//   socket.send("hello from client");
+// };
+
+// socket.onmessage = (event) => {
+//   console.log("message received: " + event.data);
+// };
 
 async function handleSubmit(formData: FormData) {
-  'use server';
-  console.log('submitting form...')
-  const message = formData.get('message');
-  socket.send(message ? message : '');
+  "use server";
+  console.log("submitting form...");
+  const message = formData.get("message");
+  socket.emit(message ? message.toString() : "");
   // Perform server-side operations with the message here
-  console.log(message);
+  // console.log(message);
 }
 
 export default function Home() {
@@ -26,7 +39,10 @@ export default function Home() {
           placeholder="Type your message here..."
           className="border p-2 rounded text-gray-900"
         />
-        <button type="submit" className="mt-2 border p-2 rounded bg-blue-500 text-white">
+        <button
+          type="submit"
+          className="mt-2 border p-2 rounded bg-blue-500 text-white"
+        >
           Send
         </button>
       </form>
--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,24 @@
-import ws from 'ws';
+import { createServer } from "http"
+import { Server } from "socket.io"
 
-const WS_PORT = 8080;
+const httpServer = createServer()
 
-const server = new ws.Server({ port: WS_PORT });
+const WS_PORT = 3500;
+
+const io = new Server(httpServer, {
+  cors: {
+      origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:3000", "http://127.0.0.1:3000"]
+  }
+})
 
 console.log(`Websocket server started on port ${WS_PORT}`);
 
-server.on('connection', (socket) => {
-  console.log(`New connection from ${socket.url}`);
-  socket.on('message', (message: string) => {
-    console.log(`Received: ${message}`);
-    socket.send(`${message}`)
+io.on('connection', (socket) => {
+  console.log(`User ${socket.id} connected`)
+  io.on('message', data => {
+    console.log(`Received: ${data}`);
+    io.emit(`server received message: ${data}`)
   });
 });
+
+httpServer.listen(WS_PORT, () => console.log(`listening on port ${WS_PORT}`))
